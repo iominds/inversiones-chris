791e1cd4efe8b926e8b8bcd924d7cbeb8ee44d09
andrick santos <andricksantos1@gmail.com>
2017-11-02 15:00:01 -0600

    optimizacion de todo el sitio web

---
+++ js/controls.js
@@ -0,0 +1,16 @@
+//$(window).resize(function () {
+//	if($(window).width() < 700){
+//		$('.bounceInLeft').removeClass('animated');
+//	}
+//});
+
+// No cargar en dispositivos moviles, hacer un condicional con el width
+//$(document).ready(function(){
+//  var ancho = $(window).width();
+//
+//  if(ancho < 767){
+//
+//
+//  }
+//  
+//});
