14f73804aec6e8334f87bff76b1dca043696d696
Andrik Santos <andricksantos1@gmail.com>
2020-05-29 22:12:58 -0600

    se cambio la pagina ya que se hara una restructuracion

--- js/funciones.js
+++
@@ -1,4 +0,0 @@
-var mostrarValor = function(){//funcion para cambiar el label en el formulario de contacto
-	var x = document.getElementById("mySelect").value;
-    document.getElementById("label").innerHTML =  x;
-	}
